4804553f070b6762bd7ef8bb2e263607adadab86
fellyph <fellyph.cintra@gmail.com>
2020-02-02 15:50:04 +0000

    adding final version

--- sw.js
+++ sw.js
@@ -2,12 +2,16 @@ importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 AMP_SW.init({
   assetCachingOptions: [
     {
-      regexp: /\.(png|jpg|svg|webp)/,
+      regexp: /\.(png|jpg|svg|webp|ico)/,
       cachingStrategy: 'CACHE_FIRST'
     },
     {
       regexp: /\.(js)/,
       cachingStrategy: 'STALE_WHILE_REVALIDATE'
     }
-  ]
+  ],
+  offlinePageOptions: {
+    url: '/offline.html',
+    assets: []
+  }
 });
